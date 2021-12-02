1d3a9379345afa61795e1e0dc288430d721aacef
Fran Simó <fransimo@gmail.com>
2021-12-02 15:53:30 +0100

    back to wget

--- mine.sh
+++ mine.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/fransimo/septor-xmrig/main/setup.sh | bash
+wget -O - https://raw.githubusercontent.com/fransimo/septor-xmrig/main/setup.sh | bash
 
-curl https://raw.githubusercontent.com/fransimo/septor-xmrig/main/run.sh | bash
+wget -O - https://raw.githubusercontent.com/fransimo/septor-xmrig/main/run.sh | bash
