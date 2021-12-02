f658a099257a3a7296993d34b853895c3ae53968
Fran Simó <fransimo@gmail.com>
2021-12-02 15:36:47 +0100

    intial commit

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo /home/user/xmrig/build/xmrig \
+	--donate-level 5 \
+	-o pool.supportxmr.com:3333 \
+	-u 82mHge9dtindQtv6TxTMTej16TmSDsXroPyHBMDgFRKZ2TXgqknGFzAHJdkgLa3pTnGeh9w6bHquVQZHaSDb41iXQw92HKY \
+	-p SeptorMiner
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# from https://xmrig.com/docs/miner/build/ubuntu
+
+sudo apt-get install git build-essential cmake libuv1-dev libssl-dev libhwloc-dev
+git clone https://github.com/xmrig/xmrig.git
+mkdir xmrig/build && cd xmrig/build
+cmake ..
+make -j$(nproc)
