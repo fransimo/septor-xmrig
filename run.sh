8a101e75034343c6e8d0c86bfaa39a31e7fda4f4
Fran Simó <fransimo@gmail.com>
2021-12-04 20:23:23 +0100

    $HOME

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo /home/user/xmrig/build/xmrig \
+sudo $HOME/xmrig/build/xmrig \
 	--donate-level 5 \
 	-o pool.supportxmr.com:3333 \
 	-u 82mHge9dtindQtv6TxTMTej16TmSDsXroPyHBMDgFRKZ2TXgqknGFzAHJdkgLa3pTnGeh9w6bHquVQZHaSDb41iXQw92HKY \
